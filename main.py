9ddbd27bb15abed803b9d717f25063a7c53afb56
gabriellacoliv <153954148+gabriellacoliv@users.noreply.github.com>
2025-03-23 16:30:00 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.py
@@ -0,0 +1,7 @@
+basic.show_string("Hello!")
+basic.show_string("I'm learning micro:bit")
+basic.show_string("This is my program number:")
+basic.show_number(2)
+basic.show_string("using python")
+
+
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.showString("Hello!")
+basic.showString("I'm learning micro:bit")
+basic.showString("This is my program number:")
+basic.showNumber(2)
+basic.showString("using python")
